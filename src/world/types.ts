22bc410cb2709f7cc84250697df78a52a3448026
Garry Xu <garry.x@outlook.com>
2026-05-02 21:57:36 +0800

    fix(pathfinding): remove incorrect minecraft: prefix from block name sets

--- src/world/types.ts
+++ src/world/types.ts
@@ -47,32 +47,32 @@ export enum BlockCategory {
 }
 
 export const CLIMBABLE_BLOCKS = new Set([
-  "minecraft:ladder", "minecraft:vine", "minecraft:weeping_vines",
-  "minecraft:twisting_vines", "minecraft:scaffolding"
+  "ladder", "vine", "weeping_vines",
+  "twisting_vines", "scaffolding"
 ]);
 
 export const WATER_BLOCKS = new Set([
-  "minecraft:water", "minecraft:flowing_water"
+  "water", "flowing_water"
 ]);
 
 export const MINEABLE_BLOCKS = new Set([
-  "minecraft:dirt", "minecraft:grass_block", "minecraft:gravel",
-  "minecraft:sand", "minecraft:stone", "minecraft:cobblestone",
-  "minecraft:netherrack", "minecraft:oak_log", "minecraft:birch_log",
-  "minecraft:spruce_log", "minecraft:jungle_log", "minecraft:acacia_log",
-  "minecraft:dark_oak_log", "minecraft:mangrove_log",
-  "minecraft:cherry_log", "minecraft:oak_leaves", "minecraft:birch_leaves",
-  "minecraft:spruce_leaves", "minecraft:jungle_leaves", "minecraft:acacia_leaves",
-  "minecraft:dark_oak_leaves", "minecraft:mangrove_leaves",
-  "minecraft:cherry_leaves",
+  "dirt", "grass_block", "gravel",
+  "sand", "stone", "cobblestone",
+  "netherrack", "oak_log", "birch_log",
+  "spruce_log", "jungle_log", "acacia_log",
+  "dark_oak_log", "mangrove_log",
+  "cherry_log", "oak_leaves", "birch_leaves",
+  "spruce_leaves", "jungle_leaves", "acacia_leaves",
+  "dark_oak_leaves", "mangrove_leaves",
+  "cherry_leaves",
 ]);
 
 export const DOOR_BLOCKS = new Set([
-  "minecraft:wooden_door", "minecraft:iron_door",
-  "minecraft:spruce_door", "minecraft:birch_door", "minecraft:jungle_door",
-  "minecraft:acacia_door", "minecraft:dark_oak_door", "minecraft:mangrove_door",
-  "minecraft:cherry_door", "minecraft:fence_gate", "minecraft:spruce_fence_gate",
-  "minecraft:birch_fence_gate", "minecraft:jungle_fence_gate",
-  "minecraft:acacia_fence_gate", "minecraft:dark_oak_fence_gate",
-  "minecraft:mangrove_fence_gate", "minecraft:cherry_fence_gate",
+  "wooden_door", "iron_door",
+  "spruce_door", "birch_door", "jungle_door",
+  "acacia_door", "dark_oak_door", "mangrove_door",
+  "cherry_door", "fence_gate", "spruce_fence_gate",
+  "birch_fence_gate", "jungle_fence_gate",
+  "acacia_fence_gate", "dark_oak_fence_gate",
+  "mangrove_fence_gate", "cherry_fence_gate",
 ]);
